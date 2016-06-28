9bef694d5291ff72aef651c689274b1a3c5a0588
vipinvats42 <vipinvats42@gmail.com>
2016-06-28 15:58:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tapesh/rourgproject/CarouselLinearLayout.java
@@ -0,0 +1,37 @@
+package com.example.tapesh.rourgproject;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by TAPESH on 6/28/2016.
+ */
+public class CarouselLinearLayout extends LinearLayout {
+
+    private float scale = CarouselPagerAdapter.BIG_SCALE;
+
+    public CarouselLinearLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CarouselLinearLayout(Context context) {
+        super(context);
+    }
+
+    public void setScaleBoth(float scale) {
+        this.scale = scale;
+        this.invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // The main mechanism to display scale animation, you can customize it as your needs
+        int w = this.getWidth();
+        int h = this.getHeight();
+        canvas.scale(scale, scale, w/2, h/2);
+    }
+}
